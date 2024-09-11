c1137645c9c28dd1d1d0956c7e3979e03dde0596
zahra <zahra20hb@gmail.com>
2024-09-11 07:58:21 -0700

    pictures uploaded and link added

--- src/components/blogComponents/BlogSlider/index.jsx
+++ src/components/blogComponents/BlogSlider/index.jsx
@@ -13,6 +13,8 @@ import { Pagination, Autoplay, EffectFade } from 'swiper/modules';
 import Image from 'next/image';
 import slide from '../../../../public/images/blog/Blog-Item.png'
 import DateAndViews from '@/components/globalComponents/DateAndViews';
+import Link from 'next/link';
+import arrow from '../../../../public/icons/Vector 9.svg'
 
 const BlogSlider = () => {
     const [date, setDate] = useState('')
@@ -44,7 +46,9 @@ const BlogSlider = () => {
                     <SwiperSlide>
                         <div className='h-screen w-full bg-slide1 bg-cover bg-no-repeat pt-[60vh]'>
                             <div className='w-full h-full p-16 text-white backdrop-brightness-50 backdrop-blur-sm'>
-                                {/* <Image src={slide} alt='' /> */}
+                                <Link href={'#'} className='absolute top-16 right-14'>
+                                    <Image src={arrow} alt=''/>
+                                </Link>
                                 <p className='text-4xl font-bold pb-3'>Lorem ipsum dolor sit amet consectetur.</p>
                                 <p>
                                     Lorem ipsum dolor sit amet consectetur. Diam id leo id elementum nibh quis ut vehicula dui. Vel ultrices commodo congue vestibulum enim neque. Sit et at rhoncus augue at. Commodo id consequat mauris et. Adipiscing morbi nulla est erat nec mauris venenatis. Vulputate dignissim urna ullamcorper porttitor suspendisse habitant id lectus. Diam sapien condimentum mattis duis morbi turpis. Fermentum nec varius arcu arcu nisi diam. Feugiat massa nibh non semper nunc tempor auctor amet. Sapien dignissim augue purus tristique orci venenatis ornare eget.
@@ -56,7 +60,9 @@ const BlogSlider = () => {
                     <SwiperSlide>
                         <div className='h-screen w-full bg-slide2 bg-cover bg-no-repeat pt-[60vh]'>
                             <div className='w-full h-full p-16 text-white backdrop-brightness-50 backdrop-blur-sm'>
-                                {/* <Image src={slide} alt='' /> */}
+                                 <Link href={'#'} className='absolute top-16 right-14'>
+                                    <Image src={arrow} alt=''/>
+                                </Link>
                                 <p className='text-4xl font-bold pb-3'>Lorem ipsum dolor sit amet consectetur.</p>
                                 <p>
                                     Lorem ipsum dolor sit amet consectetur. Diam id leo id elementum nibh quis ut vehicula dui. Vel ultrices commodo congue vestibulum enim neque. Sit et at rhoncus augue at. Commodo id consequat mauris et. Adipiscing morbi nulla est erat nec mauris venenatis. Vulputate dignissim urna ullamcorper porttitor suspendisse habitant id lectus. Diam sapien condimentum mattis duis morbi turpis. Fermentum nec varius arcu arcu nisi diam. Feugiat massa nibh non semper nunc tempor auctor amet. Sapien dignissim augue purus tristique orci venenatis ornare eget.
@@ -68,7 +74,9 @@ const BlogSlider = () => {
                     <SwiperSlide>
                         <div className='h-screen w-full bg-slide3 bg-cover bg-no-repeat pt-[60vh]'>
                             <div className='w-full h-full p-16 text-white backdrop-brightness-50 backdrop-blur-sm'>
-                                {/* <Image src={slide} alt='' /> */}
+                                 <Link href={'#'} className='absolute top-16 right-14'>
+                                    <Image src={arrow} alt=''/>
+                                </Link>
                                 <p className='text-4xl font-bold pb-3'>Lorem ipsum dolor sit amet consectetur.</p>
                                 <p>
                                     Lorem ipsum dolor sit amet consectetur. Diam id leo id elementum nibh quis ut vehicula dui. Vel ultrices commodo congue vestibulum enim neque. Sit et at rhoncus augue at. Commodo id consequat mauris et. Adipiscing morbi nulla est erat nec mauris venenatis. Vulputate dignissim urna ullamcorper porttitor suspendisse habitant id lectus. Diam sapien condimentum mattis duis morbi turpis. Fermentum nec varius arcu arcu nisi diam. Feugiat massa nibh non semper nunc tempor auctor amet. Sapien dignissim augue purus tristique orci venenatis ornare eget.
