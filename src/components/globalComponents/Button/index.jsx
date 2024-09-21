0f690f203e804296aa6ce6cd35253ede17549f18
zahra <zahra20hb@gmail.com>
2024-09-21 07:14:58 -0700

    transition added to button component

--- src/components/globalComponents/Button/index.jsx
+++ src/components/globalComponents/Button/index.jsx
@@ -1,7 +1,7 @@
 const Button = ({ text, style, onClick, disabled }) => {
     return (
         <>
-            <button onClick={onClick} disabled={disabled} className={`bg-blue text-white py-2 px-6 flex items-center justify-center gap-2 rounded-md ${style}`}>{text}</button>
+            <button onClick={onClick} disabled={disabled} className={`bg-blue transition-all duration-300 text-white py-2 px-6 flex items-center justify-center gap-2 rounded-md ${style}`}>{text}</button>
         </>
     )
 }
