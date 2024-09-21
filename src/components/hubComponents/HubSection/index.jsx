f67b3d2d6865c68122d9be5d25483c3ccb23d97c
zahra <zahra20hb@gmail.com>
2024-09-21 07:10:41 -0700

    follow button created with state

--- src/components/hubComponents/HubSection/index.jsx
+++ src/components/hubComponents/HubSection/index.jsx
@@ -1,10 +1,17 @@
+'use client'
 import Button from "@/components/globalComponents/Button"
 import Image from "next/image"
 import add from '../../../../public/icons/Add_round_duotone_line.svg'
 import heart from '../../../../public/icons/like.svg'
 import redheart from '../../../../public/icons/State=Press.svg'
+import { useState } from "react"
 
 const HubSection = ({ avatar, username, category, image, alt }) => {
+    const [isFollow, setFollow] = useState(false)
+
+    function handleFollow() {
+        setFollow(!isFollow)
+    }
     return (
         <>
             <section className="flex flex-col gap-5">
@@ -19,12 +26,14 @@ const HubSection = ({ avatar, username, category, image, alt }) => {
                         </div>
                     </div>
                     <div>
-                        <Button text={
-                            <>
-                                Follow
-                                <Image src={add} alt="follow" />
-                            </>}
-                            style={'!bg-white !text-blue border !px-4 border-blue'} />
+                        <Button
+                            onClick={handleFollow}
+                            text={
+                                <>
+                                    Follow
+                                    {!isFollow && <Image src={add} alt="follow" />}
+                                </>}
+                            style={isFollow ? '' : '!bg-white !text-blue border !px-4 border-blue'} />
                     </div>
                 </div>
                 <div className="w-full relative rounded-lg overflow-hidden flex items-center justify-center bg-gray-200">
