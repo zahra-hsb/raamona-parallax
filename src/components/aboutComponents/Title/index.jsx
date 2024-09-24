7d3dca6a04686106890f13512bb18c32dd05bf46
zahra <zahra20hb@gmail.com>
2024-09-24 05:32:43 -0700

    an style changed

--- src/components/aboutComponents/Title/index.jsx
+++ src/components/aboutComponents/Title/index.jsx
@@ -1,7 +1,7 @@
 const Title = () => {
     return (
         <>
-            <div className="text-white px-20 pt-80 pb-16 bg-about bg-cover bg-center bg-no-repeat">
+            <div className="text-white px-20 pt-96 pb-16 bg-about bg-cover bg-center bg-no-repeat">
                 <p className="absolute top-28 left-28">{`Home > About us`}</p>
                 <h1 className="text-6xl font-bold text-center">Meet the Team</h1>
             </div>
