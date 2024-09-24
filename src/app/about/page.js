16f2bff831e5c2578acbecdc55e654bc93ade0f1
zahra <zahra20hb@gmail.com>
2024-09-24 05:31:01 -0700

    officers section added to about us page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,13 @@
 import About from "@/components/aboutComponents/About"
 import Header from "@/components/globalComponents/Header"
+import Footer from "@/components/homeComponents/Footer"
 
 const about = () => {
     return (
         <>
             <Header style={'fixed w-[90%] z-50 '} />
             <About />
+            <Footer />
         </>
     )
 }
