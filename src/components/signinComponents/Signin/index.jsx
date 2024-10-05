fb62ba711ad424e277d00605043d65a7c9ce2889
zahra <zahra20hb@gmail.com>
2024-10-05 07:21:01 -0700

    username icon and password placeholder has changed

--- src/components/signinComponents/Signin/index.jsx
+++ src/components/signinComponents/Signin/index.jsx
@@ -5,6 +5,7 @@ import SocialLogin from "@/components/globalComponents/SocialLogin"
 import bg from '../../../../public/images/signin/Image.png'
 import Input from "@/components/globalComponents/Input"
 import password from '../../../../public/icons/Password.svg'
+import user from '../../../../public/icons/user.svg'
 import Button from "@/components/globalComponents/Button"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
@@ -29,9 +30,9 @@ const Signin = () => {
                             </div>
                             <form className="flex flex-col gap-2">
                                 <div className="flex flex-col gap-2 justify-between items-center">
-                                    <Input type={'text'} id={'emailUser'} label={'Email or Username'} maxLength={20} placeholder={'Enter a Email / Username'} style={'border border-gray-500'} icon={password} width={'w-full'} />
+                                    <Input type={'text'} id={'emailUser'} label={'Email or Username'} maxLength={20} placeholder={'Enter a Email / Username'} style={'border border-gray-500'} icon={user} width={'w-full'} />
 
-                                    <Input type={'password'} id={'password'} label={'Password'} maxLength={20} placeholder={'Create a password'} style={'border border-gray-500'} icon={password} width={'w-full'} />
+                                    <Input type={'password'} id={'password'} label={'Password'} maxLength={20} placeholder={'Enter your password'} style={'border border-gray-500'} icon={password} width={'w-full'} />
                                     <Button onClick={(e) => handleLogin(e)}
                                         text={'Login'} style={'w-full mt-5'} />
                                 </div>
