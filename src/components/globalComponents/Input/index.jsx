81fb0f84881288f4a70f141b32dd77ce257f9d93
zahra <zahra20hb@gmail.com>
2024-10-06 03:03:08 -0700

    forgot password page created

--- src/components/globalComponents/Input/index.jsx
+++ src/components/globalComponents/Input/index.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 
-const Input = ({ icon, label, type, maxLength, id, placeholder, style, width }) => {
+const Input = ({ icon, label, type, maxLength, id, placeholder, style, width, onChange, value }) => {
     return (
         <>
             <div className={` ${width}`}>
                 <label htmlFor={id}>{label}</label>
                 <div className="py-1 relative ">
                     <Image src={icon} alt="" className="absolute top-3 left-2" />
-                    <input type={type} maxLength={maxLength} placeholder={placeholder} id={id} className={"rounded-md shadow-input p-2 pl-10 w-full outline-none " + style} />
+                    <input value={value} onChange={onChange} type={type} maxLength={maxLength} placeholder={placeholder} id={id} className={"rounded-md shadow-input p-2 pl-10 w-full outline-none " + style} />
                 </div>
             </div>
         </>
