e6a7c82cac9133d7a1b913dbf0517a6089f45733
zahra <zahra20hb@gmail.com>
2024-10-08 06:15:21 -0700

    text area edited

--- src/components/globalComponents/Textarea/index.jsx
+++ src/components/globalComponents/Textarea/index.jsx
@@ -3,11 +3,11 @@ import Image from "next/image"
 const Textarea = ({ icon, label, maxLength, id, placeholder, disabled, style }) => {
     return (
         <>
-            <div className="">
+            <div className="mt-5">
                 <label htmlFor={id}>{label}</label>
                 <div className="py-1 relative ">
                     <Image src={icon} alt="" className="absolute top-3 left-2" />
-                    <textarea disabled={disabled} maxLength={maxLength} placeholder={placeholder} id={id} className="rounded-md shadow-input p-2 pl-10 w-full outline-none" />
+                    <textarea cols={100} disabled={disabled} maxLength={maxLength} placeholder={placeholder} id={id} className="rounded-md shadow-input p-2 pl-10 w-full outline-none min-h-32" />
                 </div>
             </div>
         </>
