af3f8a65016851258fb1f17afe3673b08cec2bb2
zahra-hsb <zahra16toa@gmail.com>
2024-12-06 06:23:58 -0800

    type property added

--- FrontEnd/src/components/globalComponents/Button/index.jsx
+++ FrontEnd/src/components/globalComponents/Button/index.jsx
@@ -1,7 +1,7 @@
-const Button = ({ text, style, onClick, disabled }) => {
+const Button = ({ text, style, onClick, disabled, type }) => {
     return (
         <>
-            <button onClick={onClick} disabled={disabled} className={`bg-blue transition-all duration-300 text-white py-2 px-6 flex items-center justify-center gap-2 rounded-md ${style}`}>{text}</button>
+            <button onClick={onClick} type={type} disabled={disabled} className={`bg-blue transition-all duration-300 text-white py-2 px-6 flex items-center justify-center gap-2 rounded-md ${style}`}>{text}</button>
         </>
     )
 }
