e3495d7608f4c5052f78978a0f0b9744b2c23b0d
zahra-hsb <zahra16toa@gmail.com>
2024-12-06 10:09:08 -0800

    login endpoint

---
+++ FrontEnd/src/app/api/login/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server"
+
+
+export async function POST(req) {
+    try {
+        const data = await req.json()
+        if(!data) {
+            throw Error('data does not exists..')
+        }
+        const res = await fetch(`${process.env.API_URL}/api/users/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+        if(!res.ok) {
+            throw Error('error while login...');
+        }
+        console.log('response login: ', await res.json());
+        return NextResponse.json({ message: '' });
+    } catch (error) {
+        return NextResponse.json(error)
+    }
+}
