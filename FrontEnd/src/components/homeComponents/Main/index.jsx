46d0ddf021a2b42ec5647365b75f1a902a89eb3f
zahra-hsb <zahra16toa@gmail.com>
2024-12-06 05:06:57 -0800

    get started button linked to login page

--- FrontEnd/src/components/homeComponents/Main/index.jsx
+++ FrontEnd/src/components/homeComponents/Main/index.jsx
@@ -74,7 +74,7 @@ const Main = () => {
             <section ref={parallaxRef} className="w-full relative bg-gradient-to-b from-[#474afe] to-[#a4f1de] overflow-hidden h-screen">
                 <h3 ref={raamona} className={` ${isLoad ? '-translate-x-10 -translate-y-10 text-[60pt]' : 'translate-x-96 translate-y-28 text-[90pt] z-50 '} z-30 transition-all duration-1000 paris text-blue font-extrabold absolute top-36 left-96 opacity-100`}>Raamona</h3>
                 <div className="relative">
-                    <button onClick={() => router.push('/signup')} className="rounded-full shadow-top absolute top-52 right-72 backdrop-blur-lg z-50 p-5 py-8 text-center flex flex-col items-center gap-2 text-blue">
+                    <button onClick={() => router.push('/signin')} className="rounded-full shadow-top absolute top-52 right-72 backdrop-blur-lg z-50 p-5 py-8 text-center flex flex-col items-center gap-2 text-blue">
                         <Image src={start} alt="" />
                         GET START
                     </button>
