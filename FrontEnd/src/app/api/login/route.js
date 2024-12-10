f432d8bd90d95d39be231e80423408f2af1cb4e5
zahra-hsb <zahra16toa@gmail.com>
2024-12-10 11:37:26 -0800

    some validations and credentials added to login endpoint

--- FrontEnd/src/app/api/login/route.js
+++ FrontEnd/src/app/api/login/route.js
@@ -4,19 +4,38 @@ import { NextResponse } from "next/server"
 export async function POST(req) {
     try {
         const data = await req.json()
-        if(!data) {
+        if (!data) {
             throw Error('data does not exists..')
         }
-        const res = await fetch(`${process.env.API_URL}/api/users/login`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-        if(!res.ok) {
-            throw Error('error while login...');
+        const username = data.username != '' ? data.username : 'unknown'
+        const email = data.email != '' ? data.email : 'unknown'
+        console.log('response login: ', JSON.stringify(data), username);
+
+        if (data.username != '') {
+            const res = await fetch(`${process.env.API_URL}/api/Users/login/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: username, email: email, password: data.password })
+            })
+            const response = await res.json()
+            console.log('=> ', response, JSON.stringify({ username: username, email: email, password: data.password }))
+            if (!res.ok) {
+                throw Error('error while login...');
+            }
+            return NextResponse.json({ message: '', response });
+        } else if (data.email != '') {
+            const res = await fetch(`${process.env.API_URL}/api/Users/login/`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: username, email: email, password: data.password })
+            })
+            const response = await res.json()
+            console.log('=> ', response, JSON.stringify({ username: username, email: email, password: data.password }))
+            if (!res.ok) {
+                throw Error('error while login...');
+            }
+            return NextResponse.json({ message: '', response });
         }
-        console.log('response login: ', await res.json());
-        return NextResponse.json({ message: '' });
     } catch (error) {
         return NextResponse.json(error)
     }
