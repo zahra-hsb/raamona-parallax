e515691a63322e0cdc42ac37b01472aa877aba74
zahra-hsb <zahra16toa@gmail.com>
2024-12-14 11:26:29 -0800

    responsive design for main and tabs section

--- FrontEnd/src/components/globalComponents/Tabs/index.jsx
+++ FrontEnd/src/components/globalComponents/Tabs/index.jsx
@@ -3,7 +3,7 @@ import Tab from "../Tab"
 const TabsComponent = ({ tabArray, selectedTab, handleSelectTab, style, border }) => {
     return (
         <>
-            <div className={`flex gap-2 border-b px-32 ${border}`}>
+            <div className={`flex sm:gap-2 border-b px- sm:px-32 ${border}`}>
                 {tabArray?.map((item, index) => (
                     <Tab style={style} disabled={index === tabArray.length - 1 ? true : false} isSelected={selectedTab === item.id} handleSelect={() => handleSelectTab(item.id)} text={item.text} key={item.id} />
                 ))}
--- FrontEnd/src/components/homeComponents/PictureContainer/index.jsx
+++ FrontEnd/src/components/homeComponents/PictureContainer/index.jsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 const PictureContainer = ({ item, isSecond }) => {
     return (
         <>
-            <div aria-hidden={isSecond} className="rounded w-max h-full basis-full">
+            <div aria-hidden={isSecond} className="rounded sm:w-max h-full sm:basis-full">
                 <Image src={item} alt="raamona" className="object-cover max-w-none" />
             </div>
         </>
