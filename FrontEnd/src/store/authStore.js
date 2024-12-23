345efab20a7425d431bcf92f32327a3996bc73db
zahra-hsb <zahra16toa@gmail.com>
2024-12-23 10:19:42 -0800

    the authentication store has created

---
+++ FrontEnd/src/store/authStore.js
@@ -0,0 +1,53 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export const useAuthStore = create()(
+    persist(
+        (set, get) => ({
+            user: null,
+            token: null,
+            setUser: (user) => set({ user }),
+            setToken: (token) => set({ token }),
+            logout: () => {
+                set({ user: null, token: null })
+            },
+            login: async (values) => {
+                try {
+                    const response = await fetch(`/api/login`, {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(values)
+                    })
+                    const result = await response.json()
+                    set({ token: result.response.access })
+                    return result
+                } catch (error) {
+                    return error
+                }
+            },
+            fetchProfile: async () => {
+                const { token } = get()
+
+                if (!token) return
+                try {
+                    const response = await fetch('/api/profile/getInfo', {
+                        method: "GET",
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    })
+                    const result = await response.json()
+                    console.log('38: ', result)
+                    set({ user: result.data })
+                } catch (error) {
+                    console.error('Failed to fetch profile:', error)
+                    throw error
+                }
+            }
+        }),
+        {
+            name: 'auth-storage'
+        }
+    )
+)
+
